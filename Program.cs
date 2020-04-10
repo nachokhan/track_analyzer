d40280228fb3f8e64cd39591564620ea7b007af9
Nachokhan <ignacio.rigoni@gmail.com>
2020-04-10 16:26:39 -0300

    Mod: Moved File Generation Methods to new Class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -92,11 +92,10 @@ namespace Test
                     var segments_list = GetSegmentsList(segmentos);
                     combinations_Body += String.Format("{0},{1},{2},{3}\n", dt, win, 0, segmentos.Count);
                         
+                    TrackFileSaver.WriteDown_SamplesForVisualize(segmentos, visualizationFile);                        
 
-                    var f = String.Format("{3}/{0}_d_{1:00}_v_{2:00}_s_00_ALL.txt", FileName, dt, win, outputDirectory);
-                    var file0 = String.Format("{4}/{0}_d_{1:00}_v_{2:00}_s_{3:00}.txt", FileName, dt, win, 0, outputDirectory);
-                    WriteDown_Segments(segmentos, file0);
-                    WriteDown_SegmentsInfo(segmentos, f);
+                    TrackFileSaver.WriteDown_SamplesInfo(segmentos, informationFile + "_SAMPLES");
+                    TrackFileSaver.WriteDown_SegmentsInfo(segmentos, informationFile + "_SEGMENTS" );
 
 
                     for (int perm = min_per; perm <= max_per; perm += inc_per)
@@ -108,11 +107,7 @@ namespace Test
                             segments_list = GetSegmentsList(segmentos);
                             combinations_Body += String.Format("{0},{1},{2},{3}\n", dt, win, perm, segmentos.Count);
 
-                            var file = String.Format("{4}/{0:00}_d_{1:00}_v_{2:00}_s_{3:00}.txt", FileName, dt, win, perm, outputDirectory);
-                            WriteDown_Segments(segmentos, file);
-
-                            double total_distance = segmentos.Sum(s => s.Distance);
-                            Console.WriteLine(file + " --- Distancia: " + total_distance + "km");
+                            TrackFileSaver.WriteDown_SamplesForVisualize(segmentos, visualizationFile);
                         }
                     }
                 }
@@ -154,86 +149,7 @@ namespace Test
             Console.WriteLine("DIST:\n---------\n{0}\n{1}\n{2}\n{3}\n", d1, d2, d3, d4);
         }
 
-        public static void WriteDown_Segments(List<Segmento> segments, string fileName)
-        {
-            const string segmentHeader = "type,latitude,longitude,alt,name,color\n";
-
-            string segmentText;
-
-            List<string> colors = new List<string>()
-                {"green","red","blue","black","orange","yellow","grey","brown"}; 
-
-            int nextColor = 0;
 
 
-            segmentText = "";
-
-            foreach(Segmento s in segments)
-            {
-                segmentText += segmentHeader;
-                foreach(Muestra m in s.Muestras)
-                {
-                    segmentText += String.Format("T,{0},{1},{2},{3},{4}\n", 
-                        m.P1.Latitude,
-                        m.P2.Longitude,
-                        m.Elevacion,
-                        s.Name, colors[nextColor]);
-                }
-
-                nextColor++;
-                if(nextColor >= colors.Count)
-                    nextColor = 0;
-            }
-
-            var todo = segmentText;
-
-            System.IO.File.WriteAllText(fileName, todo);
-        }
-    
-        public static void WriteDown_SegmentsInfo(List<Segmento> segments, string fileName)
-        {
-            const string segmentHeader = "N(T), N(S), Dist, Lat1, Long1, Elev1, Lat2, Long2, Elev2, S_ID, S_Slope, First/Last";
-
-            string segmentText;            
-
-            segmentText = "";
-
-            double total = 0;
-            int num_total=0;
-            int num_seg;
-
-            foreach(Segmento s in segments)
-            {
-                num_seg = 0;
-                foreach(Muestra m in s.Muestras)
-                {
-                    double a = 0;
-                    if( !double.IsNaN(s.PendientePromedio))
-                        a =  Math.Sign(s.PendientePromedio);
-                   
-                    segmentText += String.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}\n", 
-                        num_total,
-                        num_seg,
-                        Math.Round(total, 2),
-                        m.P1.Latitude,
-                        m.P1.Longitude,
-                        m.P1.Elevation,
-                        m.P2.Latitude,
-                        m.P2.Longitude,
-                        m.P2.Elevation,
-                        s.ID,
-                        s.PendientePromedio,
-                        a);
-
-                    total += m.Distancia;
-                    num_total++;
-                    num_seg++;
-                }
-            }
-
-            var todo = segmentHeader + segmentText;
-
-            System.IO.File.WriteAllText(fileName, todo);
-        }    
     }
 }
